d1a9eb89febb04422a55328bb141db96068eea3f
NibeshSdev <sdev.nibs@gmail.com>
2017-09-15 00:18:15 +0545

    project setup

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+    res.send('Hello world')
+});
+
+app.listen(port, function(){
+    console.log('App is listening on port: '+port);
+});
+
